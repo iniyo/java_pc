d25f14ead4f310b20cda9ffe45e9e4027fd1259f
Administrator <inizle35@gmail.com>
2022-11-27 15:20:46 +0900

    java

--- app/src/main/java/com/example/myapplication/ShowPlaceActivity.java
+++ app/src/main/java/com/example/myapplication/ShowPlaceActivity.java
@@ -105,6 +105,6 @@ public class ShowPlaceActivity extends AppCompatActivity {
                             }
                         }
                     }
-                });
+                }); //
     }
 }
